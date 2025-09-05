7040a5a84ecc70b6f9c71bff18c2906491d4b525
Jan Ole Zabel <jan.zabel@posteo.de>
2025-09-05 17:20:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+[![Build](https://github.com/UgnilJoZ/strip-slice-prefix/actions/workflows/rust.yaml/badge.svg)](https://github.com/UgnilJoZ/strip-slice-prefix/actions/workflows/rust.yaml)
+[![Crates.io](https://img.shields.io/crates/v/strip-slice-prefix.svg)](https://crates.io/crates/strip-slice-prefix)
+[![Documentation](https://docs.rs/strip-slice-prefix/badge.svg)](https://docs.rs/strip-slice-prefix/)
+[![dependency status](https://deps.rs/crate/strip-slice-prefix/0.1.0/status.svg)](https://deps.rs/crate/strip-slice-prefix/0.1.0)
+
+# strip-slice-prefix
+
+This Rust crate provides a function to strip a prefix from a slice, similar to [`String::strip_prefix`](https://doc.rust-lang.org/std/string/struct.String.html#method.strip_prefix).
+
+See the [`documentation`](https://docs.rs/strip-slice-prefix/) or this example:
+
+```rs
+use strip_slice_prefix::strip_prefix;
+
+let greetings = b"Hello world";
+let addressee = strip_prefix(greetings, b"Hello ").unwrap();
+assert_eq!(addressee, b"world");
+```
---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+#![no_std]
+
+/// Strips a prefix from a slice
+///
+/// Returns the remaining sequence if `data` starts with `prefix`.
+///
+/// If `data` does not start with this prefix, `None` is returned.
+/// 
+/// ```
+/// use strip_slice_prefix::strip_prefix;
+/// 
+/// let auth_header = b"Basic VGhlIGFuc3dlciBpcyA0Mi4K";
+/// let credentials = strip_prefix(auth_header, b"Basic ").unwrap();
+/// assert_eq!(credentials, b"VGhlIGFuc3dlciBpcyA0Mi4K");
+/// ```
+pub fn strip_prefix<'a, T>(data: &'a [T], prefix: &[T]) -> Option<&'a [T]>
+where T: Eq {
+    let mut remaining = data;
+    for prefix_char in prefix {
+        if prefix_char == remaining.first()? {
+            remaining = &remaining[1..];
+        } else {
+            return None;
+        }
+    }
+    Some(remaining)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_suffix() {
+        let result = strip_prefix(b"Hello", b"Hello").unwrap();
+        assert_eq!(result, b"");
+    }
+
+    #[test]
+    fn test_nonempty_suffix() {
+        let result = strip_prefix(b"Hello world", b"Hello").unwrap();
+        assert_eq!(result, b" world");
+    }
+
+    #[test]
+    fn test_too_long_prefix() {
+        let result = strip_prefix(b"Hell", b"Hello");
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn test_wrong_prefix() {
+        let result = strip_prefix(b"Hello", b"world");
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn test_empty_prefix() {
+        let result = strip_prefix(b"Hello", b"").unwrap();
+        assert_eq!(result, b"Hello");
+    }
+}
